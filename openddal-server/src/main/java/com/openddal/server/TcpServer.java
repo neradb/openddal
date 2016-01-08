69fcbaddcfb0d7c03f70a7f6af2a8a2847707e5b
jorgie.li <jorgie@vipshop.com>
2016-01-08 19:24:53 +0800

    submit mysql protocol implements

---
+++ openddal-server/src/main/java/com/openddal/server/TcpServer.java
@@ -0,0 +1,11 @@
+package com.openddal.server;
+
+public class TcpServer {
+    private int port;
+    private boolean portIsSet;
+    private boolean trace;
+    private boolean ssl;
+    private boolean stop;
+
+
+}
