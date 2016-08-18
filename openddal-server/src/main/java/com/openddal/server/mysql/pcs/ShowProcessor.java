765c4487cff00006c00e22692b492d9b367568b3
jorgie.li <wplatform@gmail.com>
2016-08-18 23:26:26 +0800

    showXXX管理语句实现

--- openddal-server/src/main/java/com/openddal/server/mysql/pcs/ShowProcessor.java
+++ openddal-server/src/main/java/com/openddal/server/mysql/pcs/ShowProcessor.java
@@ -8,12 +8,15 @@ import java.util.Map;
 
 import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.sql.ast.SQLName;
 import com.alibaba.druid.sql.ast.SQLStatement;
-import com.alibaba.druid.sql.ast.expr.SQLBinaryOpExpr;
-import com.alibaba.druid.sql.ast.expr.SQLBinaryOperator;
 import com.alibaba.druid.sql.ast.expr.SQLCharExpr;
 import com.alibaba.druid.sql.ast.expr.SQLIdentifierExpr;
 import com.alibaba.druid.sql.ast.statement.SQLShowTablesStatement;
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlShowAuthorsStatement;
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlShowCharacterSetStatement;
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlShowCollationStatement;
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlShowColumnsStatement;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlShowDatabasesStatement;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlShowTableStatusStatement;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlShowVariantsStatement;
@@ -21,10 +24,13 @@ import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlEvalVisitorImpl;
 import com.alibaba.druid.sql.visitor.SQLEvalVisitor;
 import com.alibaba.druid.sql.visitor.SQLEvalVisitorUtils;
 import com.alibaba.druid.util.JdbcConstants;
+import com.openddal.engine.Constants;
 import com.openddal.result.LocalResult;
 import com.openddal.result.SimpleResultSet;
+import com.openddal.server.ServerException;
 import com.openddal.server.core.QueryProcessor;
 import com.openddal.server.core.QueryResult;
+import com.openddal.server.util.ErrorCode;
 
 /**
  * @author jorgie.li
@@ -42,16 +48,21 @@ public final class ShowProcessor implements QueryProcessor {
         List<SQLStatement> stmts = SQLUtils.parseStatements(query, JdbcConstants.MYSQL);
         for (SQLStatement stmt : stmts) {
             if (stmt instanceof MySqlShowDatabasesStatement) {
-                MySqlShowDatabasesStatement s = (MySqlShowDatabasesStatement) stmts.get(0);
+                return showDatabase((MySqlShowDatabasesStatement) stmt);
             } else if (stmt instanceof MySqlShowVariantsStatement) {
-                MySqlShowVariantsStatement s = (MySqlShowVariantsStatement) stmt;
-                return showVariables(s);
+                return showVariables((MySqlShowVariantsStatement) stmt);
             } else if (stmt instanceof SQLShowTablesStatement) {
-                SQLShowTablesStatement s = (SQLShowTablesStatement) stmt;
+                return showTables((SQLShowTablesStatement) stmt);
+            } else if (stmt instanceof MySqlShowAuthorsStatement) {
+                return showAuthors((MySqlShowAuthorsStatement) stmt);
+            } else if (stmt instanceof MySqlShowCharacterSetStatement) {
+                return showCharacterSet((MySqlShowCharacterSetStatement) stmt);
+            } else if (stmt instanceof MySqlShowCollationStatement) {
+                return showCollation((MySqlShowCollationStatement) stmt);
+            } else if (stmt instanceof MySqlShowColumnsStatement) {
+                return showColumns((MySqlShowColumnsStatement) stmt);
             } else if (stmt instanceof MySqlShowTableStatusStatement) {
-                MySqlShowTableStatusStatement s = (MySqlShowTableStatusStatement) stmt;
-            } else if (stmt instanceof MySqlShowTableStatusStatement) {
-                MySqlShowTableStatusStatement s = (MySqlShowTableStatusStatement) stmt;
+                throw ServerException.get(ErrorCode.ER_NOT_ALLOWED_COMMAND, "not allowed command:" + query);
             }
         }
 
@@ -94,13 +105,100 @@ public final class ShowProcessor implements QueryProcessor {
                 Object b = where.getAttributes().get(EVAL_VALUE);
                 boolean added = b instanceof Boolean ? (Boolean) b : false;
                 if (added) {
-                    SQLIdentifierExpr col = new SQLIdentifierExpr("VARIABLE_NAME");
-                    SQLBinaryOpExpr filter = new SQLBinaryOpExpr(col, SQLBinaryOperator.Like, like);
+                    result.addRow(new Object[] { key, value });
                 }
 
             }
         }
-        //we have to convert result.
+        // we have to convert result.
+        return new QueryResult(LocalResult.read(target.getSession().getDbSession(), result, 0));
+    }
+
+    private QueryResult showDatabase(MySqlShowDatabasesStatement s) {
+        StringBuilder sql = new StringBuilder("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA ");
+        SQLExpr like = s.getLike();
+        SQLExpr where = s.getWhere();
+        if (like != null) {
+            sql.append(" WHERE SCHEMA_NAME").append("LIKE ").append(SQLUtils.toMySqlString(like));
+        } else if (where != null) {
+            sql.append(" WHERE ").append(SQLUtils.toMySqlString(where));
+        }
+        return target.process(sql.toString());
+    }
+
+    private QueryResult showTables(SQLShowTablesStatement s) {
+        StringBuilder sql = new StringBuilder(
+                "SELECT TABLE_NAME, TABLE_SCHEMA FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ");
+        String schema = Constants.SCHEMA_MAIN;
+        SQLName database = s.getDatabase();
+        SQLExpr like = s.getLike();
+        SQLExpr where = s.getWhere();
+        if (database != null) {
+            schema = SQLUtils.toMySqlString(database);
+        }
+        sql.append("'").append(schema).append("'");
+
+        if (like != null) {
+            sql.append(" AND TABLE_NAME").append("LIKE ").append(SQLUtils.toMySqlString(database));
+        } else if (where != null) {
+            sql.append(" AND ").append(SQLUtils.toMySqlString(where));
+        }
+        sql.append(" ORDER BY TABLE_NAME");
+        return target.process(sql.toString());
+    }
+
+    private QueryResult showColumns(MySqlShowColumnsStatement s) {
+        // not support like and where
+        s.setLike(null);
+        s.setWhere(null);
+        return target.process(SQLUtils.toMySqlString(s));
+    }
+
+    private QueryResult showAuthors(MySqlShowAuthorsStatement s) {
+
+        SimpleResultSet result = new SimpleResultSet();
+        result.addColumn("Variable_name", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Value", Types.VARCHAR, Integer.MAX_VALUE, 0);
+
+        // we have to convert result.
+        return new QueryResult(LocalResult.read(target.getSession().getDbSession(), result, 0));
+
+    }
+
+    private QueryResult showCharacterSet(MySqlShowCharacterSetStatement s) {
+        SimpleResultSet result = new SimpleResultSet();
+        result.addColumn("Charset", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Description", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Default collation", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Maxlen", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Description", Types.INTEGER, Integer.MAX_VALUE, 0);
+        SQLExpr like = s.getPattern();
+        SQLExpr where = s.getWhere();
+        if (like != null) {
+
+        } else if (where != null) {
+
+        }
+        // we have to convert result.
+        return new QueryResult(LocalResult.read(target.getSession().getDbSession(), result, 0));
+    }
+
+    private QueryResult showCollation(MySqlShowCollationStatement s) {
+        SimpleResultSet result = new SimpleResultSet();
+        result.addColumn("Charset", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Collation", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Id", Types.INTEGER, Integer.MAX_VALUE, 0);
+        result.addColumn("Default", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Compiled", Types.VARCHAR, Integer.MAX_VALUE, 0);
+        result.addColumn("Sortlen", Types.INTEGER, Integer.MAX_VALUE, 0);
+        SQLExpr like = s.getPattern();
+        SQLExpr where = s.getWhere();
+        if (like != null) {
+
+        } else if (where != null) {
+
+        }
+        // we have to convert result.
         return new QueryResult(LocalResult.read(target.getSession().getDbSession(), result, 0));
     }
 
